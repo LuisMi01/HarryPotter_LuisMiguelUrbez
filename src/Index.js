e1e9fc3d4dd2e224d5b9b3b6ed95b24fbe3b2b2e
LuisMi01 <lmurbezv@gmail.com>
2023-04-26 10:44:09 +0200

    .

--- src/Index.js
+++ src/Index.js
@@ -8,6 +8,7 @@ const mysql = require('mysql2');
 
 const express = require('express')
 const app = express()
+const si = require('express-handlebars');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const res = require("express/lib/response");
@@ -18,9 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false, limit: '1mb' }))
 app.use(bodyParser.json({ limit: '1mb' }))
 
 
-app.get('/', (req, res, next) => {
-    res.send("HOLA")
-})
 const pool = mysql.createPool({
     host:"localhost",
     database:"peliculas",
@@ -28,25 +26,16 @@ const pool = mysql.createPool({
     password:"LuisMiguel2303",
     connectionLimit: 10
 })
-
-pool.query('SELECT * FROM peliculas.pelicula', (err, res, fields) => {
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('Fields', fields)
-    console.log('Resultados', res)
-
-})
-pool.query('SELECT * FROM peliculas.personaje', (err, res, fields) => {
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('Fields', fields)
-    console.log('Resultados', res)
-
-})
+app.get('/', function(req, res) {
+    pool.query('SELECT * FROM peliculas.pelicula', (err, results) => {
+        if(err){
+            console.log(err);
+            res.send('Error al obtener datos');
+        } else {
+            res.json(results);
+        }
+    });
+});
 
 //app.use('/personajes', personajes)
 //app.use('/peliculas', peliculas)
