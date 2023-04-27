01851b768a48fb160cc08191a7530be9cbee24dd
LuisMi01 <lmurbezv@gmail.com>
2023-04-27 15:25:05 +0200

    API undefined

--- src/Llamadas.js
+++ src/Llamadas.js
@@ -1,3 +1,5 @@
+'use strict'
+
 async function buscarPersonaje() {
     let id = parseInt(document.getElementById('input-busqueda').value);
     if (isNaN(id)) {
@@ -30,6 +32,30 @@ async function buscarPersonaje() {
         alert('Error al buscar el personaje. Por favor, inténtelo de nuevo más tarde.');
     }
 }
+
 document.addEventListener('DOMContentLoaded', async function () {
     document.getElementById('buscar-btn').addEventListener('click', buscarPersonaje);
-});
+
+})
+
+/*
+async function cargarPersonajes() {
+    let res = await fetch('http://localhost:3003/personaje');
+    let personajes = await res.json();
+
+    let personajesDOM = document.getElementById('resultados');
+
+    try {
+        personajes.forEach(element => {
+            let li = document.createElement('li');
+            li.innerHTML = element.id;
+            personajesDOM.appendChild(li);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+window.onload = () => {
+    cargarPersonajes().then(r => console.log('Personajes cargados'));
+}*/
