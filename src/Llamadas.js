5946601cd1e09806e1f6b520b6f6c77844977e03
LuisMi01 <lmurbezv@gmail.com>
2023-04-28 16:50:33 +0200

    Funcionamiento de API REST

--- src/Llamadas.js
+++ src/Llamadas.js
@@ -1,6 +1,39 @@
-
+'use strict'
 
 async function buscarPersonaje() {
+    let resultadosDOM = document.getElementById('resultados2');
+    let id = parseInt(document.getElementById('input-busqueda2').value);
+    if (isNaN(id)) {
+        alert('Debe ingresar un número válido');
+        return;
+    }
+
+    try {
+        let res = await fetch('http://localhost:3003/personaje/' + id);
+        let personaje = await res.json();
+        console.log(personaje);
+
+        resultadosDOM.innerHTML = "";
+
+        let p2 = document.createElement('p');
+        p2.innerHTML = 'Id: ' + personaje[0].id;
+        resultadosDOM.appendChild(p2);
+
+        let p = document.createElement('p');
+        p.innerHTML = 'Nombre: ' + personaje[0].name;
+        resultadosDOM.appendChild(p);
+
+        let p3 = document.createElement('p');
+        p3.innerHTML = 'Descripcion: ' + personaje[0].desc;
+        resultadosDOM.appendChild(p3);
+
+    } catch (error) {
+        console.error(error);
+        alert("El id es incorrecto, por favor introduzca numeros entre el 0 y 64 (incluidos)")    }
+}
+
+
+async function buscarPelicula() {
     let resultadosDOM = document.getElementById('resultados');
     let id = parseInt(document.getElementById('input-busqueda').value);
     if (isNaN(id)) {
@@ -10,36 +43,44 @@ async function buscarPersonaje() {
 
     try {
         let res = await fetch('http://localhost:3003/pelicula/' + id);
-        let personaje = await res.json();
-        console.log(personaje);
+        let pelicula = await res.json();
+        console.log(pelicula);
 
-        resultadosDOM.innerText = '';
+        resultadosDOM.innerHTML = "";
 
-        let padre = document.createElement('div');
-        let pNombre = document.createElement('p');
-        pNombre.innerText = `Titulo: ${personaje.title}`;
-        let pDesc = document.createElement('p');
-        pDesc.innerText = `Duracion (min): ${personaje.length}`;
-        let pId = document.createElement('p');
-        pId.innerText = `Id pelicula: ${personaje.id}`;
-        let pDirector = document.createElement('p');
-        pDirector.innerText = `Fecha: ${personaje.year}`;
+        let p2 = document.createElement('p');
+        p2.innerHTML = 'Id: ' + pelicula[0].id;
+        resultadosDOM.appendChild(p2);
+
+        let p = document.createElement('p');
+        p.innerHTML = 'Titulo: ' + pelicula[0].title;
+        resultadosDOM.appendChild(p);
+
+        let p3 = document.createElement('p');
+        p3.innerHTML = 'Descripcion: ' + pelicula[0].sinopsis;
+        resultadosDOM.appendChild(p3);
+
+        let p4 = document.createElement('p');
+        p4.innerHTML = 'Fecha: ' + pelicula[0].year;
+        resultadosDOM.appendChild(p4);
+
+        let p5 = document.createElement('p');
+        p5.innerHTML = 'Duracion (min): ' + pelicula[0].length;
+        resultadosDOM.appendChild(p5);
 
 
-        padre.appendChild(pNombre);
-        padre.appendChild(pDesc);
-        padre.appendChild(pId);
-        padre.appendChild(pDirector);
-        resultadosDOM.appendChild(padre);
+
+
 
     } catch (error) {
         console.error(error);
-        alert('Error al buscar el personaje. Por favor, inténtelo de nuevo más tarde.');
+        alert("El id es incorrecto, por favor introduzca numeros entre el 0 y 7 (incluidos)")
     }
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
-    document.getElementById('buscar-btn').addEventListener('click', buscarPersonaje);
+document.getElementById('buscar-btn').addEventListener('click', buscarPelicula);
+    document.getElementById('buscar-btn2').addEventListener('click', buscarPersonaje);
 });
 
 
