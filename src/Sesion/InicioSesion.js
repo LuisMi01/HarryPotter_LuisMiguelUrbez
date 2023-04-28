f1cf10c9d60900977ad77313e576c4d289a08109
LuisMi01 <lmurbezv@gmail.com>
2023-04-28 23:14:05 +0200

    Terminada

--- src/Sesion/InicioSesion.js
+++ src/Sesion/InicioSesion.js
@@ -1,9 +1,7 @@
-'use strict'
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const bearerToken = require('express-bearer-token');
 
 const app = express();
@@ -13,45 +11,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bearerToken());
 
-// Función para cifrar una cadena de texto con SHA1
 function hashSHA1(text) {
     return crypto.createHash('sha1').update(text).digest('hex');
 }
 
-// Función para generar un token JWT
 function generateToken(payload) {
     return jwt.sign(payload, 'mysecretkey', { expiresIn: '2h' });
 }
-app.get('/', (req, res) => {
-    res.send('Bienvenido al localhost:3002, para moverte entre la base de datos de usuarios y contraseñas puedes usar /login y /protected')
-})
+
+const users = [
+    {
+        username: 'user1',
+        passwordHash: hashSHA1('password1')
+    },
+    {
+        username: 'user2',
+        passwordHash: hashSHA1('password2')
+    }
+];
 
 app.post('/login', (req, res) => {
-
-    res.send('Usuarios del inicio de sesion:')
     const { username, password } = req.body;
 
-    // Buscamos el usuario en la base de datos
-    const nuevoUser = users.find(user => user.username === username);
+    const nuevoUser = Users.tablaUsuarios.find(user => user.username === username);
 
     if (!nuevoUser) {
-        return res.status(401).json({ message: 'Usuario no encontrado o no existe aun' });
+        return res.status(401).json({ message: 'Usuario no encontrado' });
     }
-
-    // Comprobamos la contraseña cifrada
     const passwordHash = hashSHA1(password);
     if (nuevoUser.passwordHash !== passwordHash) {
         return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
-
-    // Generamos y devolvemos el token JWT
     const token = generateToken({ username });
     res.json({ token });
 });
 
-// Ruta protegida
 app.get('/protected', (req, res) => {
-    // Validamos el token recibido en la cabecera Authorization
     const token = req.token;
     try {
         const decoded = jwt.verify(token, 'mysecretkey');
@@ -60,5 +55,4 @@ app.get('/protected', (req, res) => {
         res.status(401).json({ message: 'Token inválido' });
     }
 });
-
 app.listen(3002)
--- src/Sesion/RecogidaDatos.js
+++ src/Sesion/RecogidaDatos.js
@@ -1,36 +1,12 @@
 'use strict'
-
-// Obtener el botón
-const boton = document.getElementById("boton-enviar");
-
-// Agregar un evento click al botón
-boton.addEventListener("click", function() {
-
-    // Obtener los valores de los campos de texto
-    const nombre = document.getElementById("nombre").value;
-    const pass = document.getElementById("password").value;
-
-
-    // Validar que se hayan ingresado valores en los campos
-    if(nombre === '' || pass === '') {
-        alert('Por favor complete todos los campos.');
-        return;
-    }
-
-    // Crear objeto con los datos ingresados
-    const datos = {
-        nombre: nombre,
-        password: pass,
-    };
-
-    // Enviar los datos al servidor (aquí debes agregar tu propia lógica para enviar los datos al servidor)
-    console.log(datos);
-    enviarDatosAlServidor(datos);
-
-});
+const recogerDatos = () => {
+    const user = document.getElementById('nombre').value;
+    const password = document.getElementById('password').value;
+    return { user, password };
+};
 
 const enviarDatosAlServidor = (datos) => {
-    fetch('https://localhost:3002/login', {
+    fetch('http://localhost:3002/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -38,26 +14,25 @@ const enviarDatosAlServidor = (datos) => {
         body: JSON.stringify(datos)
     })
         .then(response => {
-            if(response.ok) {
-                // Aquí se pueden tomar acciones en caso de que el envío sea exitoso
+            if (response.ok) {
                 console.log('Datos enviados al servidor correctamente');
+                console.log(datos)
+                window.location.href = "Combate/IndexCombate.html";
             } else {
-                // Aquí se pueden tomar acciones en caso de que el envío falle
                 console.error('Error al enviar los datos al servidor');
             }
         })
         .catch(error => {
-            // Aquí se pueden tomar acciones en caso de que ocurra un error
             console.error('Error en la petición fetch', error);
         });
-}
+};
 
-
-
-
-
-
-
+const form = document.getElementById('formulario');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const datos = recogerDatos();
+    enviarDatosAlServidor(datos);
+});
 
 
 /*
